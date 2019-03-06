8e75ea7aef82cd9c86c050e95d9f9a8b5b36a165
사용자 방문 <Guest@imac-3.local>
2019-03-06 15:46:57 +0900

    Initial Commit

---
+++ Git TestUITests/Git_TestUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Git_TestUITests.swift
+//  Git TestUITests
+//
+//  Created by 방문 사용자 on 2019. 3. 6..
+//  Copyright © 2019년 1234. All rights reserved.
+//
+
+import XCTest
+
+class Git_TestUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
